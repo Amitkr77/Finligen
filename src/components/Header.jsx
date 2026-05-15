a22c0122e7f5f9bb2721ebb5b9349a146790d05d
ayushkumar21161 <ayush@homeasy.io>
2026-05-15 12:58:01 +0530

    Updated project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,23 +26,23 @@ const Header = () => {
               </span>
             </div>
 
-            <a href="#">
+            <Link to="/Home">
               <h1 className="text-[18px] font-semibold text-[#111827] group-hover:text-[#0c3b42] group-hover:scale-103 transition">
                 FINLIGEN
               </h1>
-            </a>
+            </Link>
           </div>
 
           {/* Desktop Navigation */}
           <nav className="hidden md:flex items-center gap-10">
             {navLinks.map((link) => (
-              <a
+              <Link
                 key={link.name}
-                href={link.href}
+                to={link.href}
                 className="text-[14px] text-gray-700 font-medium hover:text-black transition duration-300"
               >
                 {link.name}
-              </a>
+              </Link>
             ))}
           </nav>
 
@@ -70,13 +71,13 @@ const Header = () => {
           <div className="md:hidden pb-5">
             <nav className="flex flex-col gap-4 pt-3">
               {navLinks.map((link) => (
-                <a
+                <Link
                   key={link.name}
-                  href={link.href}
+                  to={link.href}
                   className="text-gray-700 font-medium hover:text-black transition"
                 >
                   {link.name}
-                </a>
+                </Link>
               ))}
 
               <button className="bg-[#0c3b42] text-white py-3 rounded-full text-sm font-medium mt-2">
